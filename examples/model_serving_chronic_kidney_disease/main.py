3ec1e2d23d39543018acbcd24f94c54c87ecd68e
manuarya <marya@rocketscience.one>
2022-06-29 10:05:28 -0400

    29JuneManuArya update for main.py

--- examples/model_serving_chronic_kidney_disease/main.py
+++ examples/model_serving_chronic_kidney_disease/main.py
@@ -4,6 +4,7 @@ import pickle as pkl
 from io import StringIO
 
 import pandas as pd
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -11,7 +12,9 @@ app = FastAPI()
 
 model = pkl.load(open("kidney_disease.pkl", "rb"))
 
+list_of_usernames = list()
 
+# Class to define the request body and the type hints of each attribute
 class RequestBody(BaseModel):
     type: str
     content: str
@@ -22,12 +25,36 @@ def hello_world():
     return {"Hello": "World"}
 
 
+@app.get("/home/{user_name}")
+def write_home(user_name: str, query):
+    return {"Name": user_name, "query": query}
+
+
+@app.put("/username/{user_name}")
+def put_data(user_name: str):
+    print(user_name)
+    list_of_usernames.append(user_name)
+    return {"username": user_name}
+
+
+@app.post("/postData")
+def post_data(user_name: str):
+    list_of_usernames.append(user_name)
+    return {"usernames": list_of_usernames}
+
+
+@app.delete("/deleteData/{user_name}")
+def delete_data(user_name: str):
+    return {"usernames": list_of_usernames}
+
+
+# Endpoint to receive data for making prediction
 @app.post("/predict")
 def predict(data: RequestBody):
 
     if data.type == "csv":
         csvStringIO = StringIO(data.content)
-        to_predict = pd.read_csv(csvStringIO, sep=",", header=True)
+        to_predict = pd.read_csv(csvStringIO, sep=",", header=None)
 
     if data.type == "json":
         pass

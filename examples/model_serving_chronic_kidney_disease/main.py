bab05960d0767d82635ed28e2c3b597cea6b5aff
Eric H <ehammel@rocketscience.one>
2022-07-05 14:27:32 -0400

    remove useless code chunks

--- examples/model_serving_chronic_kidney_disease/main.py
+++ examples/model_serving_chronic_kidney_disease/main.py
@@ -9,57 +9,17 @@ import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 app = FastAPI()
 
-logging.basicConfig(filename="file.log", level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 
 model = pkl.load(open("kidney_disease.pkl", "rb"))
-# with open("test_data.csv", 'r') as file:
-#     csvreader = csv.reader(file)
-#     header=next(csvreader)
-#     for row in csvreader:
-#         print(row)
-#         row_string = ','.join(row)
-#         print(row_string)
-#         rows.append(row)
 
-# print(header)
-# print(rows)
-
-list_of_usernames = list()
 
 # Class to define the request body and the type hints of each attribute
 class RequestBody(BaseModel):
     type: str
     content: str
-
-
-@app.get("/")
-def hello_world():
-    return {"Hello": "World"}
-
-
-@app.get("/home/{user_name}")
-def write_home(user_name: str, query):
-    return {"Name": user_name, "query": query}
-
-
-@app.put("/username/{user_name}")
-def put_data(user_name: str):
-    print(user_name)
-    list_of_usernames.append(user_name)
-    return {"username": user_name}
-
-
-@app.post("/postData")
-def post_data(user_name: str):
-    list_of_usernames.append(user_name)
-    return {"usernames": list_of_usernames}
-
-
-@app.delete("/deleteData/{user_name}")
-def delete_data(user_name: str):
-    return {"usernames": list_of_usernames}
 
 
 # Endpoint to receive data for making prediction
@@ -77,4 +37,3 @@ def predict(data: RequestBody):
         pass
 
     return prediction.tolist()
-    # return data.content

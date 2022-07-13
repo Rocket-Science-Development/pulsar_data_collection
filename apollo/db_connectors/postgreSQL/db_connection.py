42af26262e58cb5a00dc16a30b0f1ff33c9e75f2
Eric H <ehammel@rocketscience.one>
2022-07-13 11:03:39 -0400

    start organizing sdk source code

---
+++ apollo/db_connectors/postgreSQL/db_connection.py
@@ -0,0 +1,45 @@
+import csv
+import logging
+import pickle as pkl
+import sqlite3 as db
+from io import StringIO
+
+import pandas as pd
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+def sqlupdate(df):
+    try:
+        conn = db.connect("SQLite_Python.db")
+        cursor = conn.cursor()
+        print("Database created and Successfully Connected to SQLite")
+
+        # cursor.execute("SELECT * FROM mpm_data_ing;")
+
+        df.to_sql("df", conn, if_exists="replace")
+
+        # print(cursor.fetchall())
+
+        # cursor.execute(
+        #     """
+        #     CREATE TABLE IF NOT EXISTS mpm_data_ing as
+        #     SELECT * FROM df
+        #     """
+        # )
+
+        df.to_sql(name="mpm_10jul", con=conn, if_exists="append")
+
+        cursor.execute("SELECT * FROM mpm_10jul;")
+        print(cursor.fetchall())
+
+    except db.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if cursor:
+            cursor.close()
+            conn.close()
+            print("The SQLite connection is closed")
+
+    return

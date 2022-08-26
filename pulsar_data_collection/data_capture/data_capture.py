2e17c5cfdb2787173d08ea5e05cf669d5342c9a2
michael <michael.makashyn@oak-tree.us>
2022-08-26 21:01:53 +0300

    Added ingestion tests

--- pulsar_data_collection/data_capture/data_capture.py
+++ pulsar_data_collection/data_capture/data_capture.py
@@ -7,10 +7,7 @@ import numpy as np
 import pandas as pd
 from pydantic import BaseModel, Field, validator
 
-# from ..db_connectors.influxdb.db_connection import StorageEngine
 from .exceptions import CustomExceptionWithMessage as e
-
-# TODO : aadd validators for both models
 
 
 class DatabaseLogin(BaseModel):
---
+++ tests/test_ingestion.py
@@ -0,0 +1,41 @@
+import pytest
+import pandas as pd
+import pickle as pkl
+from io import StringIO
+from pulsar_data_collection.data_capture import DataCapture, DataWithPrediction, DatabaseLogin
+
+
+
+class TestDataIngestionInfluxDB:
+    """
+        This test class trying to ingest data to the influxdb
+    """
+
+    def test_data_ingestion(self):
+
+        model = pkl.load(open("examples/kidney_disease_influxdb/kidney_disease.pkl", "rb"))
+
+        to_predict = pd.read_csv("examples/kidney_disease_influxdb/test_data_no_class.csv", sep=",", header=0)
+
+        prediction = model.predict(to_predict)
+
+        database_login = DatabaseLogin(db_host="localhost", db_port=8086, db_user="admin", db_password="pass123",
+                                       db_name="testDB", protocol="line", measurement="something")
+
+        dat_predict = DataWithPrediction(prediction=prediction, data_points=to_predict)
+
+        dat_capture = DataCapture(
+            storage_engine="influxdb",
+            model_id="RS101",
+            model_version="1.0",
+            data_id="FluxDB",
+            y_name="ABC",
+            pred_name="ABC",
+            operation_type="INS",
+            login_url=database_login
+        )
+
+        dat_capture.collect(dat_predict)
+
+
+

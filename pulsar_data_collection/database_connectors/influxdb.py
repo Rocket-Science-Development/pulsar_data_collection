dcb3d211ef4f80f7bc87d9956322ba474b93a140
Eric H <ehammel@rocketscience.one>
2023-02-14 16:19:59 -0500

    checking if bucket exists in database

--- pulsar_data_collection/database_connectors/influxdb.py
+++ pulsar_data_collection/database_connectors/influxdb.py
@@ -2,21 +2,24 @@ from typing import List
 
 import pandas as pd
 from database_actions import DatabaseActions, DatabaseActionsFactory
-from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
+from influxdb_client.client.influxdb_client_async import (
+    InfluxDBClient,
+    InfluxDBClientAsync,
+)
 
 
 class InfluxdbActions(DatabaseActions):
     """Infuxdb database connection"""
 
-    def make_connection(self, url: str, token: str, org: str) -> InfluxDBClientAsync:
+    def make_connection(self, url: str, token: str, org: str, bucket_name: str) -> InfluxDBClientAsync:
         """Makes connection to the Influxdb database"""
-
-        return InfluxDBClientAsync(url=url, token=token, org=org)
+        if self._check_if_bucket_exists(url=url, token=token, org=org, bucket_name=bucket_name):
+            return InfluxDBClientAsync(url=url, token=token, org=org)
 
     async def write_data(
         self,
         async_client: InfluxDBClientAsync,
-        bucket: str,
+        bucket_name: str,
         records: pd.DataFrame,
         data_frame_measurement_name: str,
         data_frame_tag_columns: List[str],
@@ -25,12 +28,17 @@ class InfluxdbActions(DatabaseActions):
         """Sends data to the database"""
         async with async_client as client:
             await client.write_api().write(
-                bucket=bucket,
+                bucket=bucket_name,
                 record=records,
                 data_frame_measurement_name=data_frame_measurement_name,
                 data_frame_tag_columns=data_frame_tag_columns,
                 data_frame_timestamp_column=data_frame_timestamp_column,
             )
+
+    def _check_if_bucket_exists(self, url: str, token: str, org: str, bucket_name: str):
+        with InfluxDBClient(url=url, token=token, org=org) as client:
+            if client.BucketsAPI().find_bucket_by_name(bucket_name=bucket_name):
+                return True
 
 
 class Influxdb(DatabaseActionsFactory):

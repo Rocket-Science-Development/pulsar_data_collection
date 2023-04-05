d2de7fc65fbcfdc95207bc75426755cbcb054eff
Eric H <ehammel@rocketscience.one>
2023-04-05 15:40:05 -0400

    commenting tests

--- examples/kidney_disease_influxdb/docker/influxdb/influxdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-influx -execute "CREATE USER ${INFLUXDB_USER} WITH PASSWORD '${INFLUXDB_USER_PASSWORD}' WITH ALL PRIVILEGES"
-
-cat >> /etc/influxdb/influxdb.conf <[http]
-  auth-enabled = true
-EOD
---
+++ tests/test_pulse.py
@@ -0,0 +1,14 @@
+# import pickle as pkl
+# from io import StringIO
+
+# import pandas as pd
+# import pytest
+
+# from pulsar_data_collection import models, pulse
+
+
+# class TestPulse:
+#     params = models.PulseParameters()
+
+#     def test_storage_engine_exists():
+#         ...

5cfa7174c0055c89c34a57f3315b6e038b79dd63
Eric H <ehammel@rocketscience.one>
2023-04-13 15:16:24 -0400

    commenting test

--- tests/test_pulse.py
+++ tests/test_pulse.py
@@ -1,41 +1,50 @@
+# # import pickle as pkl
+# # from io import StringIO
+
+# import os
 # import pickle as pkl
-# from io import StringIO
+# from pathlib import Path
 
-import os
+# import pytest
+# from dotenv import load_dotenv
 
-import pytest
+# from pulsar_data_collection import models, pulse
 
-from pulsar_data_collection import models, pulse
 
-# from unittest.mock import Mock, call, patch
+# ## env file path
+# def load_database_config():
+#     dotenv_path = Path("db/influxdb/")
+#     load_dotenv(dotenv_path=dotenv_path)
+#     db_login = {
+#         "url": os.getenv("INFLUXDB_URL", "http://localhost:8086"),
+#         "token": os.getenv("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN", "mytoken"),
+#         "org": os.getenv("DOCKER_INFLUXDB_INIT_ORG", "pulsarml"),
+#         "bucket_name": os.getenv("DOCKER_INFLUXDB_INIT_BUCKET", "demo"),
+#     }
+#     return db_login
 
 
 # @pytest.fixture
-# def mock_influxdb() -> Mock:
-#     mock_influxdb_instance = Mock(set=Mock(return_value=True))
+# def load_variables():
+#     model_path = Path("model/kidney_disease.pkl")
+#     valid_pulse_params = pulse.PulseParameters(
+#         model_id="1",
+#         model_version="v1",
+#         data_id="1",
+#         reference_data_storage="./data/clean/clean_data.csv",
+#         y_name="y_pred",
+#         storage_engine="influxdb",
+#         login=load_database_config(),
+#     )
+
+
+# @pytest.fixture(autouse=True)
+# def mock_influxdb(mocker):
+#     mock_influxdb_instance = mocker.patch.object()
 #     return mock_influxdb_instance
 
-db_login = {
-    "url": os.getenv("INFLUXDB_URL", "http://localhost:8086"),
-    "token": os.getenv("INFLUX_TOKEN", "mytoken"),
-    "org": os.getenv("INFLUXDB_ORG", "pulsarml"),
-    "bucket_name": os.getenv("INFLUXDB_BUCKET_NAME", "demo"),
-}
 
-
-@pytest.fixture
-def valid_pulse_paramaters() -> models.PulseParameters:
-    return pulse.PulseParameters(
-        model_id="1",
-        model_version="v1",
-        data_id="1",
-        reference_data_storage="./data/clean/clean_data.csv",
-        y_name="y_pred",
-        storage_engine="influxdb",
-        login=db_login,
-    )
-
-
-# class TestPulse:
-#     def test_storage_engine_exists(valid_params: models.PulseParameters):
-#         ...
+# @pytest.mark.usefixtures("load_variables", "load_database_config", "mock_influxdb")
+# class TestInfluxdb:
+#     def test_ingestion(self):
+#         model = pkl.load(open(model_path))

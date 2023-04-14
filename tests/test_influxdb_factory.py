d339419ec6618520ec4d333602885cfc01e00989
Eric H <ehammel@rocketscience.one>
2023-04-14 15:12:38 -0400

    rewriting fixtures to avoid doing too many things

--- tests/test_influxdb_factory.py
+++ tests/test_influxdb_factory.py
@@ -24,29 +24,35 @@ def storage_engine():
 
 
 @pytest.fixture(scope="session")
-def docker_compose_file(pytestconfig):
-    return Path("db/influxdb/docker-compose.yaml")
-
-
-@pytest.fixture(scope="session")
-def run_influxdb(docker_ip, docker_compose_file, docker_services):
+def db_login(docker_ip):
     load_dotenv(dotenv_path=Path("db/influxdb/"))
-    port = docker_services.port_for("influxdb", 8086)
     db_login = {
-        "url": os.getenv("INFLUXDB_URL", f"http://{docker_ip}:{port}/"),
+        "url": os.getenv("INFLUXDB_URL", f"http://{docker_ip}:8086/"),
         "token": os.getenv("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN", "mytoken"),
         "org": os.getenv("DOCKER_INFLUXDB_INIT_ORG", "pulsarml"),
         "bucket_name": os.getenv("DOCKER_INFLUXDB_INIT_BUCKET", "demo"),
     }
-
-    docker_services.wait_until_responsive(timeout=5, pause=1, check=lambda: is_responsive(**db_login))
     return db_login
 
 
-@pytest.mark.usefixtures("run_influxdb", "storage_engine")
+@pytest.fixture(scope="session")
+def docker_compose_file(pytestconfig):
+    return Path("tests/db/influxdb/docker-compose.yaml")
+
+
+@pytest.fixture(scope="session")
+def run_influxdb(db_login, docker_services):
+    docker_services.wait_until_responsive(timeout=2, pause=0, check=lambda: is_responsive(**db_login))
+
+
+@pytest.mark.usefixtures("db_login", "run_influxdb", "storage_engine")
 class TestInfluxDB:
-    def test_make_connection_is_not_none(self, run_influxdb, storage_engine):
-        db_login = run_influxdb
+    def test_make_connection_is_not_none(self, db_login, storage_engine):
         influxdb = storage_engine.get_database_actions()
         db_connection = influxdb.make_connection(**db_login)
         assert db_connection is not None
+
+    def test_make_connection_isinstance_InfluxDBClient(self, db_login, storage_engine):
+        influxdb = storage_engine.get_database_actions()
+        db_connection = influxdb.make_connection(**db_login)
+        assert isinstance(db_connection, InfluxDBClient)

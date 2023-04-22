ca3c45578a7007db2a0bb01af0b6b3089134abde
Eric H <ehammel@rocketscience.one>
2023-04-22 12:01:26 -0400

    Writing data synchronously for now

--- pulsar_data_collection/database_connectors/influxdb.py
+++ pulsar_data_collection/database_connectors/influxdb.py
@@ -1,8 +1,23 @@
+from typing import Tuple
+
 from influxdb_client import BucketsApi, InfluxDBClient
+from influxdb_client.client.exceptions import InfluxDBError
 
 from .database_actions import DatabaseActions, DatabaseActionsFactory
 
 # from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
+
+
+class BatchingCallback(object):
+    def success(self, conf: Tuple[str], data: str):
+        print(f"Written batch: {conf}")
+
+    def error(self, conf: Tuple[str], data: str, exception: InfluxDBError):
+        print(f"Cannot write batch: {conf} \n due: {exception}")
+        raise exception
+
+    def retry(self, conf: Tuple[str], data: str, exception: InfluxDBError):
+        print(f"Retryable error occurs for batch: {conf} \n retry: {exception}")
 
 
 class InfluxdbActions(DatabaseActions):
@@ -22,25 +37,30 @@ class InfluxdbActions(DatabaseActions):
         ):
             return InfluxDBClient(url=kwargs["url"], token=kwargs["token"], org=kwargs["org"])
 
-    async def write_data(self, **kwargs):
+    def write_data(self, **kwargs):
         """Write data to the database
         Parameters
         ----------
-        client: InfluxDBClientAsync
+        client: InfluxDBClient
         bucket_name: str
         records: pd.DataFrame
         data_frame_measurement_name: str
         data_frame_tag_columns: List[str]
         data_frame_timestamp_column: str
         """
-        with kwargs.client as client:
-            client.write_api().write(
-                bucket=kwargs.bucket_name,
-                record=kwargs.records,
-                data_frame_measurement_name=kwargs.data_frame_measurement_name,
-                data_frame_tag_columns=kwargs.data_frame_tag_columns,
-                data_frame_timestamp_column=kwargs.data_frame_timestamp_column,
-            )
+        db_client = kwargs["client"]
+        with db_client as client:
+            callback = BatchingCallback()
+            with client.write_api(
+                success_callback=callback.success, error_callback=callback.error, retry_callback=callback.retry
+            ) as write_api:
+                write_api.write(
+                    bucket=kwargs["bucket_name"],
+                    record=kwargs["records"],
+                    data_frame_measurement_name=kwargs["data_frame_measurement_name"],
+                    data_frame_tag_columns=kwargs["data_frame_tag_columns"],
+                    data_frame_timestamp_column=kwargs["data_frame_timestamp_column"],
+                )
 
     def _check_if_bucket_exists(self, url: str, token: str, org: str, bucket_name: str):
         connection = InfluxDBClient(url=url, token=token, org=org)

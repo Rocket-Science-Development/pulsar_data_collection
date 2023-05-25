0feba636b9af0bda5f44ffd9ac5168d5cce0b088
Eric H <ehammel@rocketscience.one>
2023-05-25 11:42:11 -0400

    add Exception raised when giving wrong login info

--- tests/test_influxdb_factory.py
+++ tests/test_influxdb_factory.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 import pytest
 from influxdb_client import InfluxDBClient
+from influxdb_client.rest import ApiException
 
 from pulsar_data_collection.config import factories
 
@@ -26,12 +27,23 @@ def storage_engine():
 
 
 @pytest.fixture(scope="session")
-def db_login(docker_ip):
+def right_db_login(docker_ip):
     db_login = {
         "url": f"http://{docker_ip}:8086/",
         "token": "mytoken",
         "org": "pulsarml",
         "bucket_name": "demo",
+    }
+    return db_login
+
+
+@pytest.fixture(scope="session")
+def wrong_db_login(docker_ip):
+    db_login = {
+        "url": f"http://{docker_ip}:8086/",
+        "token": "WrongToken",
+        "org": "WrongOrg",
+        "bucket_name": "WrongBucket",
     }
     return db_login
 
@@ -43,36 +55,36 @@ def docker_compose_file(pytestconfig):
 
 # TODO: change docker based fixture to a mock in order to make tests faster
 @pytest.fixture(scope="session")
-def run_influxdb(db_login, docker_services):
-    docker_services.wait_until_responsive(timeout=2, pause=0, check=lambda: is_responsive(**db_login))
+def run_influxdb(right_db_login, docker_services):
+    docker_services.wait_until_responsive(timeout=2, pause=0, check=lambda: is_responsive(**right_db_login))
 
 
-@pytest.mark.usefixtures("db_login", "storage_engine", "run_influxdb")
+@pytest.mark.usefixtures("right_db_login", "wrong_db_login", "storage_engine", "run_influxdb")
 class TestInfluxDBIntegration:
-    def test_make_connection_is_not_none(self, db_login, storage_engine):
+    def test_make_connection_is_not_none(self, right_db_login, storage_engine):
         influxdb = storage_engine.get_database_actions()
-        db_connection = influxdb.make_connection(**db_login)
+        db_connection = influxdb.make_connection(**right_db_login)
         assert db_connection is not None
 
-    def test_make_connection_isinstance_InfluxDBClient(self, db_login, storage_engine):
+    def test_make_connection_isinstance_InfluxDBClient(self, right_db_login, storage_engine):
         influxdb = storage_engine.get_database_actions()
-        db_connection = influxdb.make_connection(**db_login)
+        db_connection = influxdb.make_connection(**right_db_login)
         assert isinstance(db_connection, InfluxDBClient)
 
-    def test_make_connection_args_check(self, db_login, storage_engine):
+    def test_make_connection_args_check(self, right_db_login, storage_engine):
         influxdb = storage_engine.get_database_actions()
-        db_connection = influxdb.make_connection(**db_login)
+        db_connection = influxdb.make_connection(**right_db_login)
         assert db_connection.url == "http://127.0.0.1:8086/"
         assert db_connection.token == "mytoken"
         assert db_connection.org == "pulsarml"
 
-    def test_write_data_writes_data(self, storage_engine, db_login, capsys):
+    def test_write_data_writes_data(self, storage_engine, right_db_login, capsys):
         # get dataframe to write
         test_data = pd.read_csv("tests/data/split/test_data_no_class.csv", header=0).copy()
 
         # Create connections
         influxdb = storage_engine.get_database_actions()
-        db_connection = influxdb.make_connection(**db_login)
+        db_connection = influxdb.make_connection(**right_db_login)
 
         other_labels = {"environment": "test", "tag": "whatever"}
         default_tags = {"model_id": "test_id", "model_version": "test_version", "data_id": "test_data_id", **other_labels}
@@ -93,13 +105,13 @@ class TestInfluxDBIntegration:
         captured = capsys.readouterr()
         assert "Written batch" in captured.out
 
-    def test_write_data_fails(self, storage_engine, db_login, capsys):
+    def test_write_data_fails(self, storage_engine, right_db_login, capsys):
         # get dataframe to write
         test_data = pd.read_csv("tests/data/split/test_data_no_class.csv", header=0).copy()
 
         # Create connections
         influxdb = storage_engine.get_database_actions()
-        db_connection = influxdb.make_connection(**db_login)
+        db_connection = influxdb.make_connection(**right_db_login)
 
         other_labels = {"environment": "test", "tag": "whatever"}
         default_tags = {"model_id": "test_id", "model_version": "test_version", "data_id": "test_data_id", **other_labels}
@@ -120,13 +132,13 @@ class TestInfluxDBIntegration:
         captured = capsys.readouterr()
         assert "Cannot write batch:" in captured.out
 
-    def test_write_data_returns_not_none(self, storage_engine, db_login):
+    def test_write_data_returns_not_none(self, storage_engine, right_db_login):
         # get dataframe to write
         test_data = pd.read_csv("tests/data/split/test_data_no_class.csv", header=0).copy()
 
         # Create connections
         influxdb = storage_engine.get_database_actions()
-        db_connection = influxdb.make_connection(**db_login)
+        db_connection = influxdb.make_connection(**right_db_login)
         other_labels = {"environment": "test", "tag": "whatever"}
         default_tags = {"model_id": "test_id", "model_version": "test_version", "data_id": "test_data_id", **other_labels}
 
@@ -149,9 +161,19 @@ class TestInfluxDBIntegration:
         |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
         """
         influxdb.write_data(**params)
-        db_connection = influxdb.make_connection(**db_login)
+        db_connection = influxdb.make_connection(**right_db_login)
         query_api = db_connection.query_api()
         data_frame = query_api.query_data_frame(query=query2)
 
         assert data_frame is not None
         assert isinstance(data_frame, pd.DataFrame)
+
+    def test_make_connection_fails(self, storage_engine, wrong_db_login, capsys):
+        # get dataframe to write
+        influxdb = storage_engine.get_database_actions()
+
+        try:
+            db_connection = influxdb.make_connection(**wrong_db_login)
+            db_connection
+        except ApiException:
+            assert True
